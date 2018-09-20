b57cb4e066a4b9d3e0953613df1fefce40680f7a
Jonathan Vazquez <vamtha24@gmail.com>
2018-09-20 18:26:24 -0500

    Exercise 3 solution

---
+++ server-mutation/index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const TaskRepo = require('./model');
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+mongoose.connection.on('error', () => console.log('[Server] MongoDB Connection Error'));
+mongoose.connection.once('open', () => console.log('[Server] MongoDB Connection Open'));
+
+const typeDefs = gql`
+  type Task {
+    id: ID!
+    text: String
+    completed: Boolean
+  }
+
+  type Query {
+    tasks: [Task]
+  }
+
+  type Mutation {
+    addTask(text: String): Task
+  }
+`;
+
+const resolvers = {
+  Query: {
+    tasks: () => TaskRepo.find()
+  },
+  Mutation: {
+    addTask: async (_, { text }) => {
+      try {
+        const task = await new TaskRepo({text}).save();
+        return task;
+      } catch(e) {
+        throw new Error(e);
+      }
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`[Server] Server ready at ${url}`);
+});
